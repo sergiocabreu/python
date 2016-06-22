922ce7839703720da17486dfe43fa3425bc94882
sergio <sergio@TCE03_DEPINF_35.tce.ce.gov.br>
2016-06-22 18:00:28 -0300

    Primeiro Commit.

--- python/src/parsehtml/ParseHtml.py
+++ python/src/parsehtml/ParseHtml.py
@@ -3,8 +3,19 @@
 import urllib
 from bs4 import BeautifulSoup
 
-url = 'http://www.dicionariodoaurelio.com/'
+
+print("inicio")
+url='http://www.cbf.com.br/competicoes/brasileiro-serie-a/tabela/2015#.V2rx6_krKCg'
+#url = 'http://www.dicionariodoaurelio.com/'
 f = urllib.request.urlopen(url)
+
+print("recuperou url")
+
 html_doc = f.read()
+print("leu arquivo")
+
 soup = BeautifulSoup(html_doc)
-print(soup)
+print("converteu para soup")
+
+#print(soup)
+print(soup.find('table'))
