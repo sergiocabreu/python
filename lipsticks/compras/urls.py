7b2ba755e4814598f531757af923d9e85b8c34be
Sérgio C. Abreu <sergiocabreu@gmail.com>
2016-09-07 17:52:52 -0300

    Primeiro commit do django

---
+++ lipsticks/compras/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url, include
+from django.contrib import admin
+from compras import views as compras_views
+
+urlpatterns = [
+    url(r'^$', compras_views.index, name='index')
+]
