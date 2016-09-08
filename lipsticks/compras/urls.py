f5933f7ea232ca369779cd086d33b388486dee50
Sérgio C. Abreu <sergiocabreu@gmail.com>
2016-09-07 21:20:47 -0300

    Inclusão da tela de compra. Inclusão do mapeamento das url.

--- lipsticks/compras/urls.py
+++ lipsticks/compras/urls.py
@@ -3,5 +3,6 @@ from django.contrib import admin
 from compras import views as compras_views
 
 urlpatterns = [
-    url(r'^$', compras_views.index, name='index')
+    url(r'^$', compras_views.index, name='index'),
+    url(r'^compras/$', compras_views.compras, name='compras')
 ]
